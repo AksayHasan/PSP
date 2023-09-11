3d7384fae68bee33cc2a2cd430ea23b37431d419
AksayHasan <105709190+AksayHasan@users.noreply.github.com>
2023-09-11 13:02:03 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>PlaySafe Portal</title>
+        <title>UniCrypto Token</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
